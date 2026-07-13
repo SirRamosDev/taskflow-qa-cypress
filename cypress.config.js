59b46b3ce16d1c6090eb9a9644b2b53b79286f3c
Mateus Souza <maxtheu@hotmail.com>
2026-07-13 14:08:28 -0300

    add cypress github actions with artifacts

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+
+  video: true,
+
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
